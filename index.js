5134aaeeb58248230d6f75b71ed25eaa48e50b36
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2025-05-01 14:16:16 +0200

    url and param

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Worldsds");
+app.get("/user/:id?/:name?", (req, res) => {
+  if (req.params.id) {
+    res.send("User " + req.params.id + " " + req.params.name);
+  } else {
+    res.send("All users");
+  }
 });
+
+app.get("/data", (req, res) => {
+  if (req.query.search) {
+    res.send(" You searched for : " + req.query.search);
+  } else {
+    res.send("Nothing was searched");
+  }
+});
+
 app.listen(8080, () => {
   console.log("Server started");
 });
