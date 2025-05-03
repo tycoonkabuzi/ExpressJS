1a671b03ead6f81ab659d305b4414e75f4bae987
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2025-05-03 09:07:13 +0200

    conditions in hbs or view engine

--- configuration.js
+++ configuration.js
@@ -7,11 +7,19 @@ app.engine("hbs", hbs.engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {
+    title: "Home Page",
+    content:
+      "This is the home page and has most of the important things we need",
+    displayTitle: true,
+  });
 });
 
 app.get("/second", (req, res) => {
-  res.render("second");
+  res.render("second", {
+    title: "Creation",
+    content: "When we talk about creation, we often mean other things",
+  });
 });
 app.listen(port, () => {
   console.log("Server started");
